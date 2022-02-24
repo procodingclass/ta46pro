ece27fbe7ddaa81516f31a431d8f74a820d36c50
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-02-24 23:19:52 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -6,9 +6,9 @@ class Form {
     this.greetings2 = createElement("h2");
   }
   display() {
-    this.input.position(displayWidth / 2 - 90, displayHeight / 2 - 150);
+     this.input.position(380, 180);
     this.input.size(180, 30);
-    this.buttonImg.position(displayWidth / 2 + 50, displayHeight / 2 - 100);
+    this.buttonImg.position(475 + 50, 250);
     this.buttonImg.size(150, 50);
 
     this.buttonImg.mousePressed(() => {
@@ -22,8 +22,8 @@ class Form {
       console.log(player.index);
       player.addPlayer();
       player.updateCount(playerCount);
-      this.greetings.html("Hello " + player.name);
-      this.greetings.position(displayWidth / 2 - 200, displayHeight / 2 - 150);
+    this.greetings.html("Hello " + player.name);
+      this.greetings.position(300, 180);
     });
   }
 
@@ -34,24 +34,22 @@ class Form {
   }
   end() {
     
-      var reset = createImg("./images/restart.png");
-      reset.position(200, 50);
-      reset.size(150, 50);
-      reset.mousePressed(() => {
-        player.updateBikesAtEnd(0);
-        player.updateCount(0);
-        game.update(0);
-        var playerref = database.ref("players");
-        playerref.remove();
-        this.greetings2.hide();
-        window.location.reload();
-      });
-    
+      var reset = createButton("Reset");
+  reset.position(750, 20);
+  reset.mousePressed(() => {
+    player.updateBikesAtEnd(0);
+    player.updateCount(0);
+    game.update(0);
+    var playerref = database.ref("players");
+    playerref.remove();
+    this.greetings2.hide();
+    window.location.reload();
+  });
 
-    this.greetings2.html(
+      this.greetings2.html(
       "Congratulations " + player.name + " Your Rank is " + player.rank
     );
-    this.greetings2.class("greeting");
-    this.greetings2.position(displayWidth / 2 - 100, displayHeight / 2 - 50);
+    this.greetings2.position(300, 185);
+  }
   }
 }
